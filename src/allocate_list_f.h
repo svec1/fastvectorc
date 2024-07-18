edb7f36c490b8e4624e1d23d6ebda90451bf858c
svec1 <filippshvechikov228@gmail.com>
2024-07-18 13:17:58 +0300

    imp func for allocate mem of list and get data

--- src/allocate_list_f.h
+++ src/allocate_list_f.h
@@ -3,22 +3,29 @@
 
 #include <stdio.h>
 
-typedef struct node{
+#define MAX_SIZE_NODE_DATA 5
+
+typedef struct data{
     void* allocate_data;
     size_t data_size;
-
-    node* next_node;
+} data;
+typedef struct node{
+    data data_alloc;
+    struct node* next_node;
 } node;
 typedef struct root{
-    void* allocate_data;
-
-    size_t data_size;
+    data data_alloc;
     size_t data_byte_size;
-    size_t max_data_size;
 
     node* node;
 } root;
 
 typedef root list;
+
+void byte_data_copy(void** dest, void* src, size_t dest_size_byte, size_t src_size_byte, size_t offset_byte);
+void* get_data(void* src, size_t dest_size_byte, size_t data_byte_size, size_t offset_byte);
+
+void allocate_data_list(root* list_r, void* data_src, size_t data_src_byte_size);
+void* extract_data_elem(root* list_r, size_t index);
 
 #endif
--- src/main_test.c
+++ src/main_test.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "allocate_list_f.h"
 
 int main(){
 
-
+    list *l = malloc(sizeof(list));
+    l->data_byte_size = sizeof(int);
+    l->data_alloc.allocate_data = malloc(l->data_byte_size*MAX_SIZE_NODE_DATA);
+    l->data_alloc.data_size = 0;
+    int a = 12;
+    allocate_data_list(l, &a, sizeof(int));
+    void* a_ptr = extract_data_elem(l, 0);
+    printf("Int a: %i", *((int*)a_ptr));
     return 0;
 }
